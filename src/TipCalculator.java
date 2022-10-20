16c0708f1f8dcc7fcab7fc1d2d0eb0f2c808c8c5
NajamSR <114600653+NajamSR@users.noreply.github.com>
2022-10-20 06:31:49 -0400

    Update TipCalculator.java

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,28 +1,41 @@
 import java.util.Scanner;
 
 public class TipCalculator {
+
+    public static String decimalFormatter(double value) {
+        if (value % 0.1 == 0) {
+          return (value + "0");
+        } else {
+          return (Math.round(value * 100)/100.0) + "";
+        }
+    }
+  
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Hi! How many people are you having over?");
         System.out.println(" Please enter the number below.");
         int people = scan.nextInt();
         System.out.print("Great to have you! How much tip do each of you plan on paying us?");
-        System.out.println("Please enter the amount below as a whole number percentage.");
+        System.out.println(" Please enter the amount below as a whole number percentage.");
         int tipPc = scan.nextInt();
         System.out.print("Great! Now we just need you to enter the total amount you are paying for your meal, ");
         System.out.println("in dollars and cents, and we will calculate your total bill.");
         double bill = scan.nextDouble();
 
-        double billPerPerson = bill/people;
-        double tipPerPerson = billPerPerson * tipPc/100.0;
+      
+        double billPerPerson = bill / people;
+        double tipPerPerson =  (billPerPerson * tipPc/100.0);
 
+      
         System.out.println();
-        System.out.println("Okay. Here is your final bill.");
-        System.out.println("Your total bill not including tip is $" + bill + ".");
-        System.out.print("But after including the $" + (tipPc/100.0 * bill) + " tip you offered us, ");
-        System.out.println("your total bill comes out to $" + (bill * (1 + tipPc / 100.0)) + ".");
-        System.out.print("This means each person in your group is paying $" + (billPerPerson + tipPerPerson));
-        System.out.println(", including a $" + tipPerPerson + " tip paid by each person.");
-
+        System.out.print("Okay. Here is your final bill. \nAfter including the $");
+        System.out.print(decimalFormatter(tipPc/100.0 * bill));
+        System.out.print(" tip you offered us, your total bill comes out to $");
+        System.out.print(decimalFormatter(bill * (1 + tipPc / 100.0)));
+        System.out.print(". \nThis means each person in your group is paying $");
+        System.out.print(decimalFormatter(billPerPerson + tipPerPerson));
+        System.out.print(", including a $");
+        System.out.print(decimalFormatter(tipPerPerson));
+        System.out.println(" tip paid by each person.");
     }
 }
